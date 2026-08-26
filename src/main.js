5d51af7f6a08d726870cdf34366153f765fb4c8f
joshy1062-crypto <joshy1062@gmail.com>
2026-08-26 09:28:19 +0530

    Add main.js with basic love letter content

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div class="container">
+    <div class="letter">
+      <h1>For You, Always</h1>
+      <div class="content">
+        <p>Dear You,</p>
+        
+        <p>I wanted to create something special, just for you. This tiny website is my way of saying how much you mean to me.</p>
+        
+        <p>Every moment with you is precious. You light up my world in ways I never thought possible.</p>
+        
+        <div class="heart">❤️</div>
+        
+        <p>This is just the beginning. Feel free to customize this message and make it your own.</p>
+        
+        <p>Forever yours,<br>
+        <span class="signature">Your Beloved</span></p>
+      </div>
+      
+      <div class="footer">
+        <p>Made with love ❤️</p>
+      </div>
+    </div>
+  </div>
+`
